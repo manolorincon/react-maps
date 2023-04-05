8f1111bf2e3f4f24215cf3282a7bd48aa187f605
Manuel Rincón <manolorincon26@gmail.com>
2023-04-05 06:42:43 -0400

    Create context and providers template

---
+++ src/MapsApp.tsx
@@ -0,0 +1,8 @@
+import { PlacesProvider } from './context/places/PlacesProvider';
+export const MapsApp = () => {
+    return (
+        <PlacesProvider>
+            <h1>Hola Mundo</h1>
+        </PlacesProvider>
+    )
+}
